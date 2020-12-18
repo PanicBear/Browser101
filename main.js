cca5780ddffa573569b3bfcba34a23e1f70aa132
ClarXo <imp_rove@naver.com>
2020-12-18 18:03:52 +0900

    removed useless variable

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 "use-strict";
 
 let windowSizeInfo = document.querySelector(".window--size");
-let screenWidth = window.screen.width;
-let screenHeight = window.screen.height;
-let outerWidth = window.outerWidth;
-let outerHeight = window.outerHeight;
-let innerWidth = window.innerWidth;
-let innerHeight = window.innerHeight;
-let clientWidth = document.documentElement.clientWidth;
-let clientHeight = document.documentElement.clientHeight;
-
 addEventListener("resize", () => {
   windowSizeInfo.innerHTML = `
     window.screen : ${window.screen.width}, ${window.screen.height}\
