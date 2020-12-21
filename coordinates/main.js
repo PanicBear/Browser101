e81f6f54ce16fe99646314ed52fd62b30eeff14e
ClarXo <imp_rove@naver.com>
2020-12-22 05:21:06 +0900

    Q4. Coordinates Answer

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,18 +1,19 @@
-const two = document.querySelector(".two");
-const four = document.querySelector(".four");
-const info = document.querySelector(".info");
-const cursorSize = 120;
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
 addEventListener("mousemove", (event) => {
-  two.style.width = `${event.pageX + cursorSize / 2}px`;
-  two.style.height = `${event.pageY + cursorSize / 2}px`;
-  four.style.width = `${screen.width - event.pageX - cursorSize / 2}px`;
-  four.style.height = `${
-    screen.height - event.pageY - (cursorSize * 3) / 2 + 8
-  }px`;
-  info.style.left = `${event.pageX + cursorSize}px`;
-  info.style.top = `${event.pageY + cursorSize / 2}px`;
-  info.innerHTML = `
-    ${event.pageX} 
-    ${event.pageY}
-    `;
+  const x = event.clientX;
+  const y = event.clientY;
+
+  console.log(`${x} ${y}`);
+
+  vertical.style.left = `${x}px`;
+  horizontal.style.top = `${y}px`;
+  target.style.left = `${x}px`;
+  target.style.top = `${y}px`;
+  tag.style.left = `${x}px`;
+  tag.style.top = `${y}px`;
+  tag.innerHTML = `${x}px, ${y}px`;
 });
