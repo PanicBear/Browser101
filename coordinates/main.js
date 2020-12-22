5b203afff49110eca437838ee5423ca0671493d7
ClarXo <imp_rove@naver.com>
2020-12-22 20:19:42 +0900

    Q6. Coordinates modification Answer

--- coordinates/main.js
+++ coordinates/main.js
@@ -3,23 +3,19 @@ const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 
-addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalf = targetRect.width / 2;
+  addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  console.log(x, y);
-
-  vertical.style.transform = `translateX(${
-    x - document.documentElement.clientWidth / 2
-  }px)`;
-  horizontal.style.transform = `translateY(${
-    y - document.documentElement.clientHeight / 2
-  }px)`;
-  target.style.transform = `translate(${
-    x - document.documentElement.clientWidth / 2 - 60
-  }px, ${y - document.documentElement.clientHeight / 2 - 60}px)`;
-  tag.style.transform = `translate(${
-    x - document.documentElement.clientWidth / 2
-  }px, ${y - document.documentElement.clientHeight / 2}px)`;
-  tag.innerHTML = `${x}px, ${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalf}px, ${
+      y - targetHalf
+    }px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
