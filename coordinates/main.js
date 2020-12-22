052c5a5d6d457ba5e4c6bf96dda1d83b52646c5e
ClarXo <imp_rove@naver.com>
2020-12-22 20:05:13 +0900

    Q6. Coordinates modification

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,19 +1,25 @@
-const vertical = document.querySelector('.vertical');
-const horizontal = document.querySelector('.horizontal');
-const target = document.querySelector('.target');
-const tag = document.querySelector('.tag');
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
 
 addEventListener("mousemove", (event) => {
   const x = event.clientX;
   const y = event.clientY;
 
-  console.log(`${x} ${y}`);
+  console.log(x, y);
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+  vertical.style.transform = `translateX(${
+    x - document.documentElement.clientWidth / 2
+  }px)`;
+  horizontal.style.transform = `translateY(${
+    y - document.documentElement.clientHeight / 2
+  }px)`;
+  target.style.transform = `translate(${
+    x - document.documentElement.clientWidth / 2 - 60
+  }px, ${y - document.documentElement.clientHeight / 2 - 60}px)`;
+  tag.style.transform = `translate(${
+    x - document.documentElement.clientWidth / 2
+  }px, ${y - document.documentElement.clientHeight / 2}px)`;
   tag.innerHTML = `${x}px, ${y}px`;
 });
