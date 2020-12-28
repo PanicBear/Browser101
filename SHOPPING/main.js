5712785c634f32f1d72102212bcd4a0a890d2889
ClarXo <imp_rove@naver.com>
2020-12-29 06:13:14 +0900

    Q7. Shopping List Answer-2(JavaScript)

--- SHOPPING/main.js
+++ SHOPPING/main.js
@@ -1,31 +1,61 @@
-const addBtn = document.querySelector(".list--footer__btn");
-const inputMsg = document.querySelector(".list--input");
-const itemContainer = document.querySelector(".list--items");
-var itemDelBtns;
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
 
-let listObj = {};
-let listIdx = 0;
-let listText = "";
+function onAdd() {
+    // 1. 사용자가 입력한 텍스트를 받아온다
+    const text = input.value;
+    if (text === '') {
+        input.focus();
+        return;
+    }
+    // 2. 새로운 아이템을 만든다 (텍스트 + 삭제버튼)
+    const item = createItem(text);
+    // 3. items 컨테이너 안에 새로 만든 아이템을 추가한다
+    items.appendChild(item);
+    item.scrollIntoView({ block: 'center' });
+    // 4. 인풋을 초기화한다
+    input.value = "";
+    input.focus();
 
-addBtn.addEventListener("click", () => {
-    if (inputMsg.value != false) {
-        listObj = {
-            "listIdx": listIdx++,
-            "listText": inputMsg.value,
-        }
-        itemContainer.innerHTML +=
-            `
-                <div class="items--item${listIdx}">
-                <div class="items--item__text${listIdx}">${listObj.listText}</div>
-                <div class="items--item__btn${listIdx}"><i class="fas fa-trash-alt"></i></div>
-                </div>
-            `;
-        inputMsg.value = "";
-        itemDelBtns = document.querySelectorAll("[class^=items--item__btn]");
-        itemDelBtns.forEach((itemDelBtn) => {
-            itemDelBtn.addEventListener("click", (event) => {
-                event.currentTarget.parentNode.remove();
-            })
+    function createItem(text) {
+        const itemRow = document.createElement('li');
+        itemRow.setAttribute('class', 'item__row');
+
+        const item = document.createElement('div');
+        item.setAttribute('class', 'item');
+
+        const name = document.createElement('span');
+        name.setAttribute('class', 'item__name');
+        name.innerText = text;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('class', 'item__delete');
+        deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        deleteBtn.addEventListener('click', () => {
+            items.removeChild(itemRow);
         })
+
+        const itemDivider = document.createElement('div');
+        itemDivider.setAttribute('class', 'item__divider');
+
+        item.appendChild(name);
+        item.appendChild(deleteBtn);
+
+        itemRow.appendChild(item);
+        itemRow.appendChild(itemDivider);
+
+        return itemRow;
+    }
+};
+
+addBtn.addEventListener('click', () => {
+    onAdd();
+});
+
+input.addEventListener('keypress', (e) => {
+    console.log(e);
+    if (e.key === 'Enter') {
+        onAdd();
     }
 });
